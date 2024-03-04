4916e2dae1400765c057d092379287a7d35431c4
Raven Cagulang <cagulangraven@gmail.com>
2024-03-05 01:18:54 +0800

    Camera
    changed minSdkVersion to 21
    added dependencies
    made temp home screen
    started camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'camera.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +20,30 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const LoginUser(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CameraScreen()),
+            );
+          },
+          child: Text('Open Camera'),
+        ),
+      ),
     );
   }
 }
