036c1b5d8ebf3036acc9e825a3fc9b0f72b6a84d
Raven Cagulang <cagulangraven@gmail.com>
2024-03-07 19:42:02 +0800

    classify file updates
    tflite_flutter dependencies

--- lib/classify.dart
+++ lib/classify.dart
@@ -1,38 +1,77 @@
 import 'dart:typed_data';
-import 'package:tflite/tflite.dart';
+import 'package:flutter/services.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class Classifier {
-  // Load TensorFlow Lite model
+  late Interpreter _interpreter;
+  late List<String> _labels;
+
+  // Load TensorFlow Lite model and labels
   Future<void> loadModel() async {
-    await Tflite.loadModel(
-      model: 'assets/model/Ensemble_BCCE_Model.tflite',
-      labels: 'assets/model/labels.txt',
-    );
+    try {
+      // Load model
+      final interpreterOptions = InterpreterOptions();
+      _interpreter = await Interpreter.fromAsset(
+        'assets/model/Ensemble_BCCE_Model.tflite',
+        options: interpreterOptions,
+      );
+
+      // Allocate tensors
+      _interpreter.allocateTensors();
+
+      // Load labels
+      _labels = await loadLabels('assets/model/labels.txt');
+    } catch (e) {
+      print('Failed to load model: $e');
+      // Handle the error, throw, or return as needed
+    }
+  }
+
+  // Load labels from a file
+  Future<List<String>> loadLabels(String path) async {
+    try {
+      final fileData = await rootBundle.loadString(path);
+      return fileData.split('\n').map((label) => label.trim()).toList();
+    } catch (e) {
+      print('Failed to load labels: $e');
+      return <String>[];
+      // Handle the error, throw, or return as needed
+    }
   }
 
   // Perform inference on image
   Future<Map<String, dynamic>?> classifyImage(Uint8List image) async {
-    List? recognitions = await Tflite.runModelOnBinary(
-      binary: image,
-      threshold: 0.1,
-      numResults: 1,
-    );
+    try {
+      // Preprocess your image if needed
 
-    // Process recognition results
-    if (recognitions != null && recognitions.isNotEmpty) {
-      // Get the top result
-      Map<String, dynamic> result = recognitions[0];
+      // Perform inference
+      _interpreter.getInputTensors()[0].data = image.buffer.asFloat32List() as Uint8List;
+      _interpreter.invoke();
+
+      // Get the output tensor and process results
+      final output = _interpreter.getOutputTensors()[0].data;
+      final result = {'output': output, 'labels': _labels};
+
       return result;
-    } else {
+    } catch (e) {
+      print('Failed to classify image: $e');
       return null;
+      // Handle the error, throw, or return as needed
     }
   }
 
   // Dispose TensorFlow Lite model
   Future<void> disposeModel() async {
-    await Tflite.close();
+    try {
+      _interpreter.close();
+    } catch (e) {
+      print('Failed to dispose model: $e');
+      // Handle the error, throw, or return as needed
+    }
   }
 }
+
+
 
 
 
