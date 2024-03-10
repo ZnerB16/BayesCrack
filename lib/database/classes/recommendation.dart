aa22f18bb0138a625d4cde5d89f55fc69ebb2da4
ZnerB16 <149281021+ZnerB16@users.noreply.github.com>
2024-03-10 15:51:56 +0800

    Setup database and tested through main-menu crack counter

--- lib/database/classes/recommendation.dart
+++ lib/database/classes/recommendation.dart
@@ -6,4 +6,8 @@ class Recommendation{
     required this.id,
     required this.advise
   });
+  factory Recommendation.fromSQfliteDatabase(Map<String, dynamic> map) => Recommendation(
+      id: map['id']?.toInt() ?? 0,
+      advise: map['advise'] ?? ''
+  );
 }
